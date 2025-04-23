ad50ebf1c804db95fdea211fe06e51308131b7c6
Edenfield <EDENFIELD@sefl.net>
2025-04-22 21:37:48 -0400

    Initial layout prior to adding in request pull

--- main.py
+++ main.py
@@ -1,8 +1,61 @@
 import pandas as pd
 import json
+import requests
 
 
-#test git push
+def getFormData() :
+    #hit 'fillout' Rest API
+    #convert json into DICT and pass to manipulateDict() function.
+    pass
 
 
-#dev git test
+def manipulateDict(data:dict) -> dict : 
+    for key, value in data.items() :
+        #print(f"KEY : {key}, Value : {value}")
+        data[key] = str(value).replace("[", "").replace("]", "").replace("{","").replace("}", "")
+    return data
+
+
+#Convert JSON data into Dictionary.
+with open("temp/testJson.json", 'r') as x:
+    toDictionary = json.load(x)
+    x.close()
+
+#assign finalize dictionary to be converted to xlsx
+toDictionary = manipulateDict(toDictionary)
+
+#normalize data-types
+df = pd.json_normalize(toDictionary)
+#convert data frame to excel file
+df.to_excel("temp/testExcel.xlsx")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

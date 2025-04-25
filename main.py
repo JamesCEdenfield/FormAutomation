17401d0881308cc84a4bf831454612f605e51615
Edenfield <EDENFIELD@sefl.net>
2025-04-24 22:56:48 -0400

    General functionality working. Hits Rest API - retrieves data - manipulates data - exports to excel file

--- main.py
+++ main.py
@@ -2,32 +2,78 @@ import pandas as pd
 import json
 import requests
 
-
-def getFormData() :
-    #hit 'fillout' Rest API
-    #convert json into DICT and pass to manipulateDict() function.
-    pass
-
-
-def manipulateDict(data:dict) -> dict : 
-    for key, value in data.items() :
-        #print(f"KEY : {key}, Value : {value}")
-        data[key] = str(value).replace("[", "").replace("]", "").replace("{","").replace("}", "")
-    return data
-
-
-#Convert JSON data into Dictionary.
-with open("temp/testJson.json", 'r') as x:
-    toDictionary = json.load(x)
+#read config file
+with open('config/config.json', 'r') as x:
+    config = json.load(x)
     x.close()
 
-#assign finalize dictionary to be converted to xlsx
-toDictionary = manipulateDict(toDictionary)
+EXCEL_EXPORT_FILE_PATH = config["excelPath"]
+API_KEY =  config["apiKey"]
+HEADERS = {
+    "Authorization" : f"Bearer {API_KEY}"
+}
 
+def getFormData() :
+
+    try : 
+        #get json data
+        response = requests.get("https://api.fillout.com/v1/api/forms", headers=HEADERS)
+
+        if response.status_code == 200:
+            print("Request successful!")
+            #get current formId
+            formID = response.json()[0]["formId"]
+            try :
+                formResponse = requests.get(f"https://api.fillout.com/v1/api/forms/{formID}/submissions", headers=HEADERS)
+                if response.status_code == 200:
+                    return formResponse.json()['responses']
+                else :
+                    print(f"Request failed with status code: {response.status_code}")
+                    print(response.text)
+            except Exception as e :
+                print("FAILED to get form data")
+                print(e)
+            #returns list of responses with questions for each
+            
+        else :
+            print(f"Request failed with status code: {response.status_code}")
+            print(response.text)
+    
+    except Exception as e :
+        print("FAILED to get form data")
+        print(e)
+
+
+
+def formatJsonForDF (data) :
+    retData = []
+    i = 1
+    #loop through submissions for current form.
+    for each in data :
+        #label to seperate submissions.
+        curResponse ='RESPONSE # '  + str(i)
+        retData.append({'Question' : curResponse })
+        #loop through each question
+        for question in each['questions'] :
+            #print(f"{question['name']} : {question['value']} " )
+            #create new row with current question/answer
+            retData.append({'Question' : question['name'], 'Answer' : question['value'] })
+        retData.append({})
+        i+=1
+    return retData    
+
+
+print("-- FORM AUTOMATION JOB STARTED --")
+#get form data
+data = getFormData() 
+#format json to prepare for excel file
+data = formatJsonForDF(data)   
 #normalize data-types
-df = pd.json_normalize(toDictionary)
+df = pd.json_normalize(data)
 #convert data frame to excel file
-df.to_excel("temp/testExcel.xlsx")
+df.to_excel(EXCEL_EXPORT_FILE_PATH)
+print(f"Excel File Created : ({EXCEL_EXPORT_FILE_PATH})")
+print("-- JOB COMPLETED --")
 
 
 
